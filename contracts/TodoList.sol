d96c2395e8e5af6f3c929b875105cd59c9465d60
Himanshu Pathak <himanshupat95@gmail.com>
2022-02-08 23:14:39 +0530

    creating events && initial run successful

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,6 +13,12 @@ contract TodoList {
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
   constructor() public {
     createTask("life is good");
   }
@@ -20,6 +26,7 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
   }
 
 
